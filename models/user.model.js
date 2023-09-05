5be629f5a6d7bd9f7bc8d37424fea1e35b4dea9e
nrjs <125939301+narjess-ben-khlifa@users.noreply.github.com>
2023-09-05 02:07:09 +0100

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,40 +1,55 @@
 const mongoose=require("mongoose")
-const bcrypt = require('bcrypt');
-const {isEmail}=require('validator')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const { isEmail } = require('validator')
 
-const UserSchema = new mongoose.Schema( 
-  {
-    name: {type: String,required:[true,"first name is required"], minlength: [2, "{PATH} must be at least 2 chars long"]},
-    alias:{type:String,required:[true,"last name is required "], minlength: [2, "{PATH} must be at least 2 chars long"]},
-    email: { type: String, required: [true, "Email is required"], minlength: [3, "L'{PATH} doit comporter au moins 3 caractères"],unique:[true,"Email already exist"],validate:[isEmail,"please enter a valid email"]},
-    password:{type:String,required:[true,"password is required"], minlength: [8, "{PATH} must be at least 8 chars long"]},
-    ideasAdded:{type:Number},
-    ideasFavorited:{type:Number}
-    
-  },{timestamps:true}
-)
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    alias: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "Email already exists"],
+        validate: [isEmail, "Please enter a valid email"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be 8 characters or longer"]
+    },
+    ideasAdded: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Idea"
+    }],
+    ideasFavorited: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Idea"
+    }]
+}, { timestamps: true });
 
+UserSchema.virtual('confirmPassword')
+    .get(() => this.confirmPassword)
+    .set(value => this.confirmPassword = value)
 
-UserSchema.virtual('cpassword')
-  .get( () => this._cpassword )
-  .set( value => this._cpassword = value );
-
-  UserSchema.pre('validate', function(next) {
-    if (this.password !== this.cpassword) {
-      this.invalidate('cpassword', 'Password MUST MATCH confirmPassword');
+UserSchema.pre('validate', function (next) {
+    if (this.password != this.confirmPassword) {
+        this.invalidate('confirmPassword', 'Password must match confirm password')
     }
-    next();
-  });
+    next()
+})
 
-  UserSchema.pre('save', function(next) {
+UserSchema.pre('save', function (next) {
     bcrypt.hash(this.password, 10)
-      .then(hash => {
-        this.password = hash;
-        next();
-      });
-  });
-  
-  
+        .then(hash => {
+            this.password = hash;
+            next()
+        })
+})
 
-
-module.exports=mongoose.model("User",UserSchema)
+module.exports = mongoose.model("User", UserSchema)
