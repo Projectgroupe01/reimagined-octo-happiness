336227f4bbe78b3b38595ec09f50fc34a9870a52
nrjs <125939301+narjess-ben-khlifa@users.noreply.github.com>
2023-09-05 02:08:08 +0100

    Update user.controllers.js

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,128 +1,59 @@
-const User =require("../models/user.model")
-const jwt = require("jsonwebtoken")
-
-const SECRET = process.env.SECRET_KEY
-const bcrypt = require ('bcrypt')
-
+const User = require("../models/user.model")
+const jwt = require("jsonwebtoken");
+const secret = process.env.FIRST_SECRET_KEY;
+const bcrypt = require('bcrypt')
 
 
-const payload = {
-    id: User._id
-  };
-   
-  
-const userToken = jwt.sign(payload, SECRET);
-
-//CRUD
-
-module.exports={
-    findAllUser:(req,res)=>
-{
-    User.find({})
-    .then(allUsers=>
-        {
-            console.log(allUsers)
-            res.json(allUsers)
-        })
-    .catch(err=>
-        {
-            res.json({message:'wait a minute😒😒',error:err})
-        })
-
-},
-//CREATE
-createNewUser : (req,res) =>
-{
-   User.create(req.body)
-        .then(user=>
-            {
-                console.log(user)
-                res.status(200).json({user})
-            })
-        .catch(err=>
-            {
-                res.status(400).json({message:'wait a minute😶😶',error:err})
-            })
-},
-
-// //FIND ONE
-findUser : (req,res) =>
-{
-   User.findOne({_id:req.params.pid})
-        .then(theUser=>
-            {
-                console.log(theUser)
-                res.json({theUser})
-            })
-        .catch(err=>
-            {
-                res.json({message:'wait a minute😶😶',error:err})
-            })
-},
-
-
-
-////register
-
-
-register: (req, res) => {
-    console.log(req.body)
-    User.create(req.body)
-
-      .then(user => {
-          const userToken = jwt.sign({
-              id: user._id
-          }, process.env.SECRET_KEY);
-   
-          res
-              .cookie("usertoken", userToken, {
-                  httpOnly: true
-              })
-              .json({ msg: "success!", user: user });
-      })
-      .catch(err => res.json(err));
-  },
-
-
-
-
-///login
-login: async(req, res) => {
-    const user = await User.findUser({ email: req.body.email });
-     
-    if(user === null) {
-    // email not found in users collection
-    return res.sendStatus(400);
-   }
-     
-    // if we made it this far, we found a user with this email address
-    // let's compare the supplied password to the hashed password in the database
-    const correctPassword = await bcrypt.compare(req.body.password, user.password);
-     
-    if(!correctPassword) {
-    // password wasn't a match!
-    return res.sendStatus(400);
-    }
-     
-    // if we made it this far, the password was correct
-    const userToken = jwt.sign({
-    id: user._id
-    }, process.env.SECRET_KEY);
-     
-   // note that the response object allows chained calls to cookie and json
-    res
-    .cookie("usertoken", userToken, {
-    httpOnly: true
-    })
-    .json({ msg: "success!" });
+module.exports = {
+    register: async (req, res) => {
+        try {
+            const potentialUser = await User.findOne({ email: req.body.email })
+            if (potentialUser) {
+                res.status(400).json({ msg: "Email exists" })
+            } else {
+                const newUser = await User.create(req.body);
+                const userToken = jwt.sign({ _id: newUser.id, email: newUser.email, name: newUser.name, displayName: newUser.displayName }, secret, { expiresIn: "1d" });
+                res.cookie("userToken", userToken, { httpOnly: false }).json({ msg: "Create new userToken success!", user: newUser })
+            }
+        } catch (err) {
+            console.log(err);
+            return res.status(400).json(err)
+        }
     },
-
-
+    login: async (req, res) => {
+        try {
+            const user = await User.findOne({ email: req.body.email })
+            if (user) {
+                const passwordMatch = await bcrypt.compare(req.body.password, user.password)
+                if (passwordMatch) {
+                    const userToken = jwt.sign({ _id: user.id, email: user.email, name: user.name, displayName: user.displayName, ideasFavorited: user.ideasFavorited }, secret, { expiresIn: "1d" });
+                    res.cookie("userToken", userToken, { httpOnly: false }).json({ msg: "Login success!", user: user })
+                } else {
+                    res.status(400).json({ message: "Invalid login attempt" })
+                }
+            } else {
+                res.status(400).json({ message: "Invalid login attempt" })
+            }
+        } catch (err) {
+            console.log(err);
+            return res.status(400).json(err)
+        }
+    },
     logout: (req, res) => {
-        res.clearCookie('usertoken');
-        res.sendStatus(200);
+        res.clearCookie("userToken").json({ message: "Logout success!" });
     }
-    
-    
 }
 
+
+module.exports.findAllUsers = (req, res) => {
+    User.find()
+        .populate("ideasAdded ideasFavorited")
+        .then(allUsers => res.json({ user: allUsers }))
+        .catch(err => res.status(400).json({ message: "Something went worng finding all users", error: err }))
+}
+module.exports.findOneUser = (req, res) => {
+    User.findById(req.params.id)
+        .populate("ideasAdded ideasFavorited")
+        .then(oneUser => res.json({ user: oneUser }))
+        .catch(err => res.status(400).json({ message: "Something went worng finding a user", error: err }))
+}
