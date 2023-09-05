e98cbf95c07f3c1afbf8424a2f3774648b15bd95
nrjs <125939301+narjess-ben-khlifa@users.noreply.github.com>
2023-09-05 02:04:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
-const express = require ("express") 
-const cors =require ("cors")
-const app= express() 
-const PORT = 5000
+const express = require('express');
+const app = express();
+const port = 8000;
+const cors = require('cors')
 const cookieParser = require('cookie-parser');
 
-
-
-//--Middleware
-app.use(cookieParser());
-app.use(express.json(),express.urlencoded({extended:true})) 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
-
-
 require('dotenv').config();
+require('./config/mongoose.config');
 
+app.use(cookieParser())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json(), express.urlencoded({ extended: true }));
 
+require('./routes/user.routes')(app);
+require('./routes/idea.routes')(app);
 
-require("./Config/mongoose.config")
-
-require("./routes/idea.routes")(app)
-require("./routes/user.routes")(app)
-
-
-
-
-
-
-app.listen(PORT,()=>
-{
-    console.log(` >>>>>💕✅ SERVER IS RUNNING ON PORT ${PORT}`)
-
-})
+app.listen(port, () => console.log(`Listening on port: ${port}`));
